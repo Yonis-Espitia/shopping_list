be5d0d89c12afe9d008922a0314a4fed826727f4
WIN10 <yezo2905@gmail.com>
2021-12-25 15:46:12 -0500

    inicie mi repositorio

---
+++ lib/MainScreens/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xff2c2b50),
+        primaryColorDark: Color(0xff050028),
+        primaryColorLight: Color(0xff050028),
+        accentColor: Color(0xffedecee),
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+      body: Center(),
+    );
+  }
+}
